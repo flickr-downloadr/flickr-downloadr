5da9d9145d38192467a36616a408266ac25d2c2a
Floyd Pink <imbleedingme@googlemail.com>
2012-06-01 03:20:27 -0400

    publish ready
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- FloydPink.Flickr.Downloadr.UI/Properties/AssemblyInfo.cs
+++ FloydPink.Flickr.Downloadr.UI/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FloydPink.Flickr.Downloadr.UI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("flickr downloadr")]
+[assembly: AssemblyDescription("A desktop application for windows that would help download all (or selected) photos from the user's photostream (in one of the selected sizes) along with the tags, titles and descriptions.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
-[assembly: AssemblyProduct("FloydPink.Flickr.Downloadr.UI")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2012")]
+[assembly: AssemblyCompany("floydpink")]
+[assembly: AssemblyProduct("flickr downloadr")]
+[assembly: AssemblyCopyright("© Haridas Pachuveetil 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
--- FloydPink.Flickr.Downloadr/Properties/AssemblyInfo.cs
+++ FloydPink.Flickr.Downloadr/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FloydPink.Flickr.Downloadr")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("floydpink")]
 [assembly: AssemblyProduct("FloydPink.Flickr.Downloadr")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2012")]
+[assembly: AssemblyCopyright("© Haridas Pachuveetil 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: InternalsVisibleTo("FloydPink.Flickr.Downloadr.Tests")]
