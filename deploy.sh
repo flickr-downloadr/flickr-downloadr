2c997db4cce6a8ce655709cb75f5625689c79ca3
Floyd Pink <imbleedingme@googlemail.com>
2012-12-27 01:16:36 -0500

    changed the commit from deploy to use SSH
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,52 @@
+SSH_ENV="$HOME/.ssh/environment"
+
+# start the ssh-agent
+function start_agent {
+    echo "Initializing new SSH agent..."
+    # spawn ssh-agent
+    ssh-agent | sed 's/^echo/#echo/' > "$SSH_ENV"
+    echo succeeded
+    chmod 600 "$SSH_ENV"
+    . "$SSH_ENV" > /dev/null
+    ssh-add
+}
+
+# test for identities
+function test_identities {
+    # test whether standard identities have been added to the agent already
+    ssh-add -l | grep "The agent has no identities" > /dev/null
+    if [ $? -eq 0 ]; then
+        ssh-add
+        # $SSH_AUTH_SOCK broken so we start a new proper agent
+        if [ $? -eq 2 ];then
+            start_agent
+        fi
+    fi
+}
+
+# check for running ssh-agent with proper $SSH_AGENT_PID
+if [ -n "$SSH_AGENT_PID" ]; then
+    ps -ef | grep "$SSH_AGENT_PID" | grep ssh-agent > /dev/null
+    if [ $? -eq 0 ]; then
+  test_identities
+    fi
+# if $SSH_AGENT_PID is not properly set, we might be able to load one from
+# $SSH_ENV
+else
+    if [ -f "$SSH_ENV" ]; then
+  . "$SSH_ENV" > /dev/null
+    fi
+    ps -ef | grep "$SSH_AGENT_PID" | grep ssh-agent > /dev/null
+    if [ $? -eq 0 ]; then
+        test_identities
+    else
+        start_agent
+    fi
+fi
+
 cd source/bin/Release
 PATH=$HOME/bin:$ADDPATH:$PATH
-REPO=https://flickr-downloadr-ci@github.com/flickr-downloadr/flickr-downloadr.git
+REPO=git@github.com:flickr-downloadr/flickr-downloadr.git
 MSG="application (v${BUILDNUMBER})"
 mkdir tmp-gh-pages
 cd tmp-gh-pages
@@ -10,7 +56,7 @@ git config push.default tracking
 cp -r ../../Deploy/* ./downloads/latest
 git add .
 git commit -m "deploying $MSG" -s
-git push --repo $REPO
+git push
 cd ../..
 rm -rf tmp-gh-pages
 echo "deployed $MSG successfully"
