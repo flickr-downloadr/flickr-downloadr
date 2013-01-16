e9f35d749f0a6f79848618bd14e9afe6672868ec
Floyd Pink <imbleedingme@googlemail.com>
2013-01-16 00:32:06 -0500

    deploying application (v0.6.4.4)
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- source/CommonAssemblyInfo.cs
+++ source/CommonAssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyrightAttribute("Copyright © 2012-2013 Haridas Pachuveetil")]
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
-[assembly: AssemblyVersionAttribute("0.6.4.3")]
-[assembly: AssemblyFileVersionAttribute("0.6.4.3")]
+[assembly: AssemblyVersionAttribute("0.6.4.4")]
+[assembly: AssemblyFileVersionAttribute("0.6.4.4")]
 [assembly: AssemblyInformationalVersionAttribute("0.6")]
 
